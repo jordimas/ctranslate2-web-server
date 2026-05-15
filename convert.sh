2ffd079a0e5d723746b2f472c15fbf7eb229e308
Jordi Mas <jmas@softcatala.org>
2026-05-15 21:39:30 +0200

    low

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-uv run ct2-transformers-converter --model google/gemma-4-31b-it --output_dir "${MODELS_DIR:-/models}/google-gemma-4-31b-it" --quantization int8 --force
+uv run ct2-transformers-converter --low_cpu_mem_usage --model google/gemma-4-31b-it --output_dir "${MODELS_DIR:-/models}/google-gemma-4-31b-it" --quantization int8 --force
